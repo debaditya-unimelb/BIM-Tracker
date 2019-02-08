d9affa92c6fa9505da428c5a29580099957bfefb
Debaditya Acharya <acharyad@student.unimelb.edu.au>
2019-02-08 18:14:04 +1100

    Update makeR3.m

--- makeR3.m
+++ makeR3.m
@@ -18,18 +18,3 @@ Ry = [cos(ry) 0 -sin(ry);
 Rz = [cos(rz) sin(rz) 0;
      -sin(rz) cos(rz) 0;
           0       0     1];
-
-% changed parameters
-% Rx = [1     0       0;
-%       0  cos(rx) -sin(rx);
-%       0 sin(rx) cos(rx)];
-% 
-% Ry = [cos(ry) 0 sin(ry);
-%          0     1    0;
-%       -sin(ry) 0  cos(ry)];
-%   
-% Rz = [cos(rz) -sin(rz) 0;
-%      sin(rz) cos(rz) 0;
-%           0       0     1];
-R = Rz*Ry*Rx;
-% R=R';
